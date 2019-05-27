cbd93a745e297069dcdc2dc93754828c50749065
Chanti <tgtechjava@gmail.com>
2019-05-27 18:29:57 +0530

    JS session4

---
+++ JS/JS Apps/one.js
@@ -0,0 +1,12 @@
+function positiveornegative()
+{
+    var num=eval(prompt('Enter a number',''))
+      if(num>0)
+      {
+         alert(num+' is positive number')
+      }
+      else
+      {
+         alert(num+' is negative number')
+      }
+}
